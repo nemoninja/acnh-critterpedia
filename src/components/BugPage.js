10056feb1ac1a2c2b52231ed996e1a94a20348e4
nemoninja <35476293+nemoninja@users.noreply.github.com>
2024-09-07 19:51:47 +0800

    BugLocationUtil?

--- src/components/BugPage.js
+++ src/components/BugPage.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 import { BugEntries } from "./BugEntries.js";
-import BugLocationsEnum from "../data/BugLocationsEnum.js";
+import { BugLocationsEnum, BugLocationUtil } from "../data/BugLocationsEnum.js";
 
 export function BugPage({ month }) {
-  const arrLocations = Object.values(BugLocationsEnum());
+  const excludedLocations = BugLocationUtil.excludeFromSections();
+  const sectionLocations = Object.values(BugLocationsEnum).filter(
+    (location) => !excludedLocations.includes(location)
+  );
 
   return (
     <div id="bug-page" key="bug-page" style={{ padding: "20px" }}>
-      {arrLocations.map((location) => (
+      {sectionLocations.map((location) => (
         <div id={`${location}-section`} key={`${location}-section`}>
           <h2 style={{ alignText: "center", width: "100%" }}>{location}</h2>
           <BugEntries location={location} month={month} />
--- src/data/BugLocationsEnum.js
+++ src/data/BugLocationsEnum.js
@@ -1,26 +1,39 @@
-const BugLocationsEnum = () =>
-  Object.freeze({
-    BeachShells: "Beach (as shells)",
-    Flying: "Flying",
-    FlyingByLight: "Flying (by light)",
-    FlyingHybridFlowers: "Flying (hybrid flowers)",
-    HittingRocks: "Hitting rocks",
-    OnBeachRocks: "On beach rocks",
-    OnFlowers: "On flowers",
-    OnFlowersWhite: "On flowers (white)",
-    OnPondsRivers: "On ponds and rivers",
-    OnRocksRain: "On rocks (rain)",
-    OnRottenFood: "On rotten food",
-    OnGround: "On the ground",
-    OnGroundRollingSnowballs: "On the ground (rolling snowballs)",
-    OnTrashItems: "On trash items",
-    OnTreeStumps: "On tree stumps",
-    OnTrees: "On trees",
-    OnTreesCoconut: "On trees (coconut)",
-    ShakingTrees: "Shaking trees",
-    UnderTreesAsLeafs: "Under trees (as leafs)",
-    Underground: "Underground",
-    VillagersHeads: "Villagers' heads",
-  });
+const BugLocationsEnum = Object.freeze({
+  BeachShells: "Beach (as shells)",
+  Flying: "Flying",
+  FlyingByLight: "Flying (by light)",
+  FlyingHybridFlowers: "Flying (hybrid flowers)",
+  HittingRocks: "Hitting rocks",
+  OnBeachRocks: "On beach rocks",
+  OnFlowers: "On flowers",
+  OnFlowersWhite: "On flowers (white)",
+  OnPondsRivers: "On ponds and rivers",
+  OnRocksRain: "On rocks (rain)",
+  OnRottenFood: "On rotten food",
+  OnGround: "On the ground",
+  OnGroundRollingSnowballs: "On the ground (rolling snowballs)",
+  OnTrashItems: "On trash items",
+  OnTreeStumps: "On tree stumps",
+  OnTrees: "On trees",
+  OnTreesCoconut: "On trees (coconut)",
+  ShakingTrees: "Shaking trees",
+  UnderTreesAsLeafs: "Under trees (as leafs)",
+  Underground: "Underground",
+  VillagersHeads: "Villagers' heads",
+});
 
-export default BugLocationsEnum;
+const BugLocationUtil = {
+  excludeFromSections: () => {
+    const enumValues = BugLocationsEnum;
+    return [
+      enumValues.FlyingByLight,
+      enumValues.FlyingHybridFlowers,
+      enumValues.FlyingByLight,
+      enumValues.OnFlowersWhite,
+      enumValues.OnGroundRollingSnowballs,
+      enumValues.OnTreesCoconut,
+    ];
+  },
+};
+
+export { BugLocationsEnum, BugLocationUtil };
